e549e228c8e511a8178591ae8e3e4fb147aea68b
bowoon <haioer@naver.com>
2024-08-10 13:15:38 +0900

    [git rebase] test-9

--- app/practice/src/main/java/com/bowoon/practice/ui/activities/MainActivity.kt
+++ app/practice/src/main/java/com/bowoon/practice/ui/activities/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // test-6
+        // test-8
 
         binding.apply {
             lifecycleOwner = this@MainActivity
